b7baa3431f130ff5751c71010e720339c8ffe3e4
bhawna <bhawnabudhiraja1993@gmail.com>
2019-05-04 17:52:16 +0530

    added threadgroup methods

---
+++ src/ThreadGroupMethods.java
@@ -0,0 +1,23 @@
+
+public class ThreadGroupMethods {
+
+	public static void main(String[] args)
+	{
+		ThreadGroup g=new ThreadGroup("Parent thread group");
+		//t1 and t2's priority is 5 by default 
+		Thread t1=new Thread(g,"thread-1");
+		Thread t2=new Thread(g,"thread-2");
+
+		System.out.println(g.getMaxPriority());
+		g.setMaxPriority(3);
+
+		/*after setting the priority of the thread group,
+		the priority of newly added threads will change and previous
+		thread's priority will remain as it was.
+		 */
+		Thread t3=new Thread(g,"thread-3");
+		System.out.println(t3.getPriority());//3
+		System.out.println(t1.getPriority());//5
+		System.out.println(t2.getPriority());//5
+	}
+}
