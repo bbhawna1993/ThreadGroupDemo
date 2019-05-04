30803ce9caad8090fc05f8819a6afa72d4208fb2
bhawna <bhawnabudhiraja1993@gmail.com>
2019-05-04 15:18:06 +0530

    add group

--- src/ThreadGroupDemo.java
+++ src/ThreadGroupDemo.java
@@ -5,7 +5,10 @@ public class ThreadGroupDemo {
 		// TODO Auto-generated method stub
 		System.out.println(Thread.currentThread().getThreadGroup().getName());
 		System.out.println(Thread.currentThread().getThreadGroup().getParent().getName());
-		System.out.println(new ThreadGroup("first group").getParent().getName());
+		//SSince this line of creating the thread group is executed by main thread
+		//therefore,it becomes the child group of main thread group.
+		ThreadGroup g=new ThreadGroup("first group");
+		System.out.println(g.getParent().getName());
 	}
 
 }
