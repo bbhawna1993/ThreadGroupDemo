21bb103c97c557d87b12b40a55b28c6ff41992a6
bhawna <bhawnabudhiraja1993@gmail.com>
2019-05-04 15:15:57 +0530

    thread group changed

--- src/ThreadGroupDemo.java
+++ src/ThreadGroupDemo.java
@@ -4,6 +4,8 @@ public class ThreadGroupDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(Thread.currentThread().getThreadGroup().getName());
+		System.out.println(Thread.currentThread().getThreadGroup().getParent().getName());
+		System.out.println(new ThreadGroup("first group").getParent().getName());
 	}
 
 }
