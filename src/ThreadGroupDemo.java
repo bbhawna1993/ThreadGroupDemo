f93c1dc4b18e2b7704ba4ceed08a33f14291776c
bhawna <bhawnabudhiraja1993@gmail.com>
2019-05-04 14:52:21 +0530

    add thread group

---
+++ src/ThreadGroupDemo.java
@@ -0,0 +1,9 @@
+
+public class ThreadGroupDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Thread.currentThread().getThreadGroup().getName());
+	}
+
+}
